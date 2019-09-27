e7d4d2c286f23722cfcd48c33c4b7038a8b0d7f1
Max <mashkovmax@gmail.com>
2019-09-27 19:42:22 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftQ",
-            dependencies: []),
+            dependencies: [
+                "Vapor",
+                "Redis",
+            ]),
         .testTarget(
             name: "SwiftQTests",
             dependencies: ["SwiftQ"]),
